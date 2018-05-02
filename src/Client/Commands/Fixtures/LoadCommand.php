6b43a2368b948ee5272bc2dbdcdf740117936d08
Tim Wagner <t.wagner@techdivision.com>
2018-05-02 17:35:08 +0200

    Fixed PSR-2 errors

--- src/Client/Commands/Fixtures/LoadCommand.php
+++ src/Client/Commands/Fixtures/LoadCommand.php
@@ -72,16 +72,17 @@ class LoadCommand extends Command
             ->addOption(InputOptionKeys::PORT, null, InputOption::VALUE_OPTIONAL, 'The default port of the host to execute the commands on', 9023)
             ->addOption(InputOptionKeys::HOSTNAME, null, InputOption::VALUE_OPTIONAL, 'The default host name to execute the commands on', '127.0.0.1')
             ->addOption(InputOptionKeys::APPLICATION_NAME, null, InputOption::VALUE_REQUIRED, 'The application name, if the command is NOT executed from the applications root directory')
-            ->setHelp(<<<EOT
-The <info>%command.name%</info> command loads data fixtures from your application:
-  <info>php %command.full_name%</info>
-You need to specify the DI names of the fixtures with the <info>--fixtures=MyFixtures</info> option:
-  <info>php %command.full_name% --fixtures=MyFixtures1 --fixtures=MyFixtures2</info>
-If you want to append the fixtures instead of flushing the database first you can use the <info>--append</info> option:
-  <info>php %command.full_name% --append</info>
-By default Doctrine Data Fixtures uses DELETE statements to drop the existing rows from
-the database. If you want to use a TRUNCATE statement instead you can use the <info>--purge-with-truncate</info> flag:
-  <info>php %command.full_name% --purge-with-truncate</info>
+            ->setHelp(
+                <<<EOT
+                The <info>%command.name%</info> command loads data fixtures from your application:
+                  <info>php %command.full_name%</info>
+                You need to specify the DI names of the fixtures with the <info>--fixtures=MyFixtures</info> option:
+                  <info>php %command.full_name% --fixtures=MyFixtures1 --fixtures=MyFixtures2</info>
+                If you want to append the fixtures instead of flushing the database first you can use the <info>--append</info> option:
+                  <info>php %command.full_name% --append</info>
+                By default Doctrine Data Fixtures uses DELETE statements to drop the existing rows from
+                the database. If you want to use a TRUNCATE statement instead you can use the <info>--purge-with-truncate</info> flag:
+                  <info>php %command.full_name% --purge-with-truncate</info>
 EOT
             );
     }
